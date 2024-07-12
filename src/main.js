6c0a219d8a7994fe1cf640498f2dfa170528e3a7
jimi-chhatrala <jimichhatrala8488@gmail.com>
2024-07-12 22:35:34 +0530

    Vue Jobs Web App Completed

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import "./assets/main.css";
+import "primeicons/primeicons.css";
+import "vue-toastification/dist/index.css";
+
+import { createApp } from "vue";
+
+import Toast from "vue-toastification";
+import router from "./router";
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(Toast);
+app.mount("#app");
